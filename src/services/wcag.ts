1f5a15dd2c56a5d9705fccd1f83d710098a96d7b
fr-froussel <4551879+fr-froussel@users.noreply.github.com>
2022-11-23 11:36:29 +0100

    Add WCAG contrast computation for 2.0 and 2.1

--- src/services/wcag.ts
+++ src/services/wcag.ts
@@ -1,3 +1,5 @@
+import { get as colorStringGet } from 'color-string';
+
 enum WCAGVersion {
     v2_0 = "2.0",
     v2_1 = "2.1",
@@ -20,27 +22,79 @@ class WCAGService {
         textColor: string,
         textSize: number,
         textBold: boolean) {
+        const contrasts = [];
+
         if (wcagVersion === WCAGVersion.v2_0) {
-            return this.checkWCAG20TextContrast(textBackgroundColor, textColor, textSize, textBold);
+            const textContrast = this.checkWCAG20TextContrast(wcagVersion, textBackgroundColor, textColor, textSize, textBold);
+            contrasts.push({
+                wcagVersion,
+                result: textContrast
+            });
+        } else if (wcagVersion === WCAGVersion.v2_1) {
+            const textContrast = this.checkWCAG20TextContrast(wcagVersion, textBackgroundColor, textColor, textSize, textBold);
+            contrasts.push({
+                wcagVersion,
+                result: textContrast
+            });
         }
-        return undefined;
+
+        return {
+            contrasts,
+            textBackgroundColor,
+            textColor,
+            textSize,
+            textBold
+        };;
     }
     //#endregion Public
 
+    //#region Utils
+    private static normalizeColorForTheRelativeLuminanceComputation(color: number): number {
+        // https://www.w3.org/TR/WCAG20/#relativeluminancedef
+        return color <= 0.03928 ? color / 12.92 : Math.pow(((color + 0.055) / 1.055), 2.4);
+    }
+
+    private static getRelativeLuminance(red: number, green: number, blue: number): number {
+        // https://www.w3.org/TR/WCAG20/#relativeluminancedef
+        const RsRGB = red / 255;
+        const GsRGB = green / 255;
+        const BsRGB = blue / 255;
+
+        const R = this.normalizeColorForTheRelativeLuminanceComputation(RsRGB);
+        const G = this.normalizeColorForTheRelativeLuminanceComputation(GsRGB);
+        const B = this.normalizeColorForTheRelativeLuminanceComputation(BsRGB);
+
+        return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    }
+    //#endregion Utils
+
     //#region Private WCAG 2.0
     private static checkWCAG20TextContrast(
+        wcagVersion: string,
         textBackgroundColor: string,
         textColor: string,
         textSize: number,
         textBold: boolean
     ) {
-        /*
-            https://www.w3.org/TR/WCAG20/#contrast-ratiodef
-            contrast ratio = (L1 + 0.05) / (L2 + 0.05), where
-            - L1 is the relative luminance of the lighter of the colors, and
-            - L2 is the relative luminance of the darker of the colors.
-        */
-        return "coucou";
+        const isLargeText = (textSize >= 18) || (textSize === 14 && textBold);
+
+        const textBackgroundColorRgb = colorStringGet(textBackgroundColor);
+        const textColorRgb = colorStringGet(textColor);
+
+        if (textBackgroundColorRgb && textColorRgb) {
+            const bkgLuminance = this.getRelativeLuminance(textBackgroundColorRgb.value[0], textBackgroundColorRgb.value[1], textBackgroundColorRgb.value[2]);
+            const textLuminance = this.getRelativeLuminance(textColorRgb.value[0], textColorRgb.value[1], textColorRgb.value[2]);
+            const L1 = bkgLuminance > textLuminance ? bkgLuminance : textLuminance;
+            const L2 = bkgLuminance > textLuminance ? textLuminance : bkgLuminance;
+            const contrastRatio = (L1 + 0.05) / (L2 + 0.05); // https://www.w3.org/TR/WCAG20/#contrast-ratiodef
+            return {
+                value: contrastRatio,
+                minimum: isLargeText ? contrastRatio > 3 : contrastRatio > 4.5,
+                enhanced: isLargeText ? contrastRatio > 4.5 : contrastRatio > 7,
+                isLargeText
+            };
+        }
+        return undefined;
     }
 
 
@@ -50,6 +104,21 @@ class WCAGService {
     //#endregion Private WCAG 2.0
 
     //#region Private WCAG 2.1
+    private static checkWCAG21TextContrast(
+        wcagVersion: string,
+        textBackgroundColor: string,
+        textColor: string,
+        textSize: number,
+        textBold: boolean
+    ) {
+        return this.checkWCAG20TextContrast(
+            wcagVersion,
+            textBackgroundColor,
+            textColor,
+            textSize,
+            textBold
+        );
+    }
 
     private static checkWCAG21() {
         return "WCAG 2.1";
